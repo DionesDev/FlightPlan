c140605990fd39c606e8fb5cda5c5ab84466fbf6
jonesmondejar24 <jonesmondejar24@gmail.com>
2023-11-26 01:22:14 +0800

    fix imports

--- js/index.js
+++ js/index.js
@@ -1,14 +1,32 @@
-function myFunction() {
-    var x = document.getElementById("nav-list");
-    var y = document.getElementById("navbar");
+// function myFunction() {
+//     var x = document.getElementById("nav-list");
+//     var y = document.getElementById("navbar");
 
-    if (x.className === "nav-list" ) {
-      x.className += " responsive";
-    } 
-    else if( y.className === "navbar"){
-        y.className += " responsive";
-    }else {
-      x.className = "nav-list";
-      y.className = "navbar";
-    }
-  }
+//     if (x.className === "nav-list" ) {
+//       x.className += " responsive";
+//     } 
+//     else if( y.className === "navbar"){
+//         y.className += " responsive";
+//     }else {
+//       x.className = "nav-list";
+//       y.className = "navbar";
+//     }
+//   }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerMenu = document.getElementById('burger-menu');
+  const navList = document.querySelector('nav ul');
+
+  burgerMenu.addEventListener('click', function () {
+      navList.classList.toggle('show');
+  });
+
+    // Add event listeners to each navigation item
+    const navItems = document.querySelectorAll('nav ul li a');
+    navItems.forEach(function (item) {
+        item.addEventListener('click', function () {
+            // Hide the navigation list when an item is clicked
+            navList.classList.remove('show');
+        });
+    });
+});
